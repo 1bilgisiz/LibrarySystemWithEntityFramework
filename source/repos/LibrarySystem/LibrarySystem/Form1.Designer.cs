f80645bb3340ffc092e391bbd0f7c83f17133358
1bilgisiz <bilgicsaniye66@gmail.com>
2023-03-06 02:08:34 +0300

    Entity frame work basic structers apply

---
+++ source/repos/LibrarySystem/LibrarySystem/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace LibrarySystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtkitapad = new System.Windows.Forms.TextBox();
+            this.BtnKitapAra = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtyazarad = new System.Windows.Forms.TextBox();
+            this.BtnYazarAra = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(50, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1051, 267);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtkitapad);
+            this.groupBox1.Controls.Add(this.BtnKitapAra);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(50, 317);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 295);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txtkitapad
+            // 
+            this.txtkitapad.Location = new System.Drawing.Point(128, 68);
+            this.txtkitapad.Name = "txtkitapad";
+            this.txtkitapad.Size = new System.Drawing.Size(136, 20);
+            this.txtkitapad.TabIndex = 2;
+            // 
+            // BtnKitapAra
+            // 
+            this.BtnKitapAra.BackColor = System.Drawing.Color.IndianRed;
+            this.BtnKitapAra.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnKitapAra.Location = new System.Drawing.Point(72, 182);
+            this.BtnKitapAra.Name = "BtnKitapAra";
+            this.BtnKitapAra.Size = new System.Drawing.Size(166, 53);
+            this.BtnKitapAra.TabIndex = 1;
+            this.BtnKitapAra.Text = "Kitap Ara";
+            this.BtnKitapAra.UseVisualStyleBackColor = false;
+            this.BtnKitapAra.Click += new System.EventHandler(this.BtnKitapAra_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(26, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kitap Adı : ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtyazarad);
+            this.groupBox2.Controls.Add(this.BtnYazarAra);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(387, 317);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(298, 295);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox1";
+            // 
+            // txtyazarad
+            // 
+            this.txtyazarad.Location = new System.Drawing.Point(128, 68);
+            this.txtyazarad.Name = "txtyazarad";
+            this.txtyazarad.Size = new System.Drawing.Size(136, 20);
+            this.txtyazarad.TabIndex = 2;
+            // 
+            // BtnYazarAra
+            // 
+            this.BtnYazarAra.BackColor = System.Drawing.Color.IndianRed;
+            this.BtnYazarAra.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnYazarAra.Location = new System.Drawing.Point(72, 182);
+            this.BtnYazarAra.Name = "BtnYazarAra";
+            this.BtnYazarAra.Size = new System.Drawing.Size(166, 53);
+            this.BtnYazarAra.TabIndex = 1;
+            this.BtnYazarAra.Text = "Yazar Ara";
+            this.BtnYazarAra.UseVisualStyleBackColor = false;
+            this.BtnYazarAra.Click += new System.EventHandler(this.BtnYazarAra_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(26, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Yazar Adı : ";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(936, 317);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(165, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Töre Sivrioğlu Alan Ögrenciler";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(925, 559);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ara";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(936, 353);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(84, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Kitap Alanlar";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(936, 386);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(126, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "En az bir kitap alanlar";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(936, 425);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(174, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "400 üzeri kitapların alınma sayısı";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(936, 460);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(144, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Telim etmeyen Ögrenciler";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 828);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtkitapad;
+        private System.Windows.Forms.Button BtnKitapAra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtyazarad;
+        private System.Windows.Forms.Button BtnYazarAra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+    }
+}
+
---
+++ source/repos/LibrarySystem/LibrarySystem/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Contexts;
+
+
+namespace LibrarySystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        DbKutuphaneSyetemEntities1 db = new DbKutuphaneSyetemEntities1();
+
+
+        private void BtnKitapAra_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection baglanti = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=DbKutuphaneSyetem;Integrated Security=True");
+
+            dataGridView1.DataSource = db.kitap.Where(x => x.kitapadi == txtkitapad.Text).ToList();
+
+
+        }
+
+        private void BtnYazarAra_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.yazar.Where(x => x.yazarad == txtyazarad.Text).ToList();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)       // 2) "Töre Sivrioğlu" nun herhangi bir kitabını alan öğrencileri bulunuz. 
+            {
+                var ogrenciler = db.ogrenci
+                    .Where(o => o.islem.Any(i => i.kitap.kitapadi == "Töre Silvioğlu")).ToList();
+
+                dataGridView1.DataSource = ogrenciler;
+            }
+
+
+
+            if (radioButton2.Checked == true)      // 5) Erkeklerin ve kızların kitap alma sayıları nelerdir
+            {
+                var islemstudents = db.ogrenci .Where(o => o.islem.Any()).GroupBy(o => o.cinsiyet)
+                    .Select  (g => new  {
+                        Cinsiyet = g.Key,
+                        Sayi = g.Count()
+                    }).ToList();
+
+                dataGridView1.DataSource = islemstudents;
+
+            }
+          
+
+
+            if (radioButton3.Checked == true)    //1) En az bir kitap almış öğrencileri bulunuz
+            {
+                var kitapalan = db.ogrenci.Where(x => x.islem.Any()).ToList();
+                dataGridView1.DataSource = kitapalan;
+
+            }
+
+
+
+            if (radioButton4.Checked == true)    //4) 400 ve üzeri sayfa sayısına sahip kitapları 2023 yılına kadar alınma sayısı kaçtır
+            {
+                var teslim = db.kitap.Where(x => x.sayfasayisi >= 400).Select(a => new
+                {
+                    Kitapadı = a.kitapadi,
+
+                    AlmaSayısı = a.islem.Count(y=>y.atarih.Value.Year <= 2023)
+
+                }).ToList();
+
+                dataGridView1.DataSource = teslim;
+            }
+
+
+
+            if(radioButton5.Checked == true)  // 3) Bilim Kurgu türünde kitap alıp teslim etmeyen öğrencileri bulunuz.
+            {
+
+                var teslimolmayan = db.ogrenci.Where(x => x.islem.Any(i => i.kitap.tur.turadi == "Bilim Kurgu" && i.vtarih == null)).ToList();
+                                    
+
+                dataGridView1.DataSource = teslimolmayan;  
+            }
+
+
+
+        }
+
+       
+
+    }
+}
